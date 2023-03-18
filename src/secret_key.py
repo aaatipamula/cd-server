3c0d22921719e0e3b41e76f63700c008bb09596a
Aniketh <aaatipamula@gmail.com>
2023-03-18 15:19:41 +0000

    updated hmac hash and default key length

--- src/secret_key.py
+++ src/secret_key.py
@@ -1,12 +1,20 @@
-import hashlib
+#!/usr/bin/python3
+
+from hashlib import sha256
 import secrets
 import hmac
-import os
 
-def gen_key(*, length: int = 256):
+def gen_key(*, length: int = 128) -> str:
     return secrets.token_urlsafe(length)
 
-def compare(client_key, server_hash):
-    client_hash ="sha256=" + hmac.new(client_key, digestmod=hashlib.sha256).hexdigest()
+def compare(client_key: bytes, msg: str, server_hash: str) -> bool:
+    client_hash ="sha256=" + hmac.new(client_key, msg, digestmod=sha256).hexdigest()
     return hmac.compare_digest(client_hash, server_hash)
 
+def export_key() -> str:
+    SECRET_KEY = gen_key()
+    print(SECRET_KEY)
+    return SECRET_KEY
+
+if __name__ == "__main__":
+    export_key()
--- src/test.py
+++
@@ -1,5 +0,0 @@
-import subprocess as sp
-
-run = sp.run(['docker', 'ps'], capture_output=True)
-
-print(run.stderr.decode())
