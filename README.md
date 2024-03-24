f4009caa2f8d9a17b3ce4cf14b8e62cf407aa567
Aniketh <aaatipamula@gmail.com>
2024-03-23 20:56:05 -0500

    updated setup with ngix config file and commands

--- README.md
+++ README.md
@@ -6,11 +6,6 @@
 DEV_FOLDER # Path to directory to host files
 SECRET_KEY # Key to verify, use src/secret_key.py to generate
 ```
-## Forward Github Webhooks
-
-```sh
-nohup gh webhook forward --repo=aaatipamula/reponame --events=push --url=http://localhost:5000/webhooks &
-```
 ## Host Locally
 
 Setup `venv`
@@ -20,7 +15,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 ```
 
-Install Dependencies
+Install dependencies
 
 ```sh
 pip install -r requirements.txt
@@ -30,21 +25,27 @@ pip install gunicorn
 Start Gunicorn
 
 ```sh
-nohup gunicorn -w 4 'src/main:app' > gunicorn.log 2>&1 &
+nohup gunicorn -w 4 'server.main:app' > gunicorn.log 2>&1 &
 ```
 
-or create a service
+Or create a service
 
 ```sh
-./scripts/createsocket.sh <username>
-sudo systemctl start gunicorn.socket
-sudo systemctl enable gunicorn.socket
+./scripts/createsocket.sh YOUR-USERNAME
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
 ```
 
-# Running
+# Development Server
 
-## Devlopment
+## Start Server
 
 ```sh
-python3 -m flask --app src/main.py run
+python3 -m flask --app server.server run
+```
+
+## Forward Github Webhooks
+
+```sh
+nohup gh webhook forward --repo=aaatipamula/REPOSITORY-NAME --events=pull_request --url=http://localhost:5000/webhooks &
 ```
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,15 @@
 
 socketfile=gunicorn.socket
 servicefile=gunicorn.service
-
-servicedir=/etc/systemd/system/
+nginxfile="cd_server"
 
 key=$(python3 ./server/secret.py)
 devfolder=$(pwd | sed 's/\/[a-zA-Z0-1_-]\+$/\/projects/')
 
 clean() {
-  rm gunicorn.* .env
+  echo "Removing service and socket files, env, and nginx config file"
+  rm gunicorn.* .env $nginxfile
+  echo "Removing python virtual env"
   rm -rf .venv/
   echo "Permanently removing $devfolder"
   rm -rf $devfolder
@@ -17,9 +18,9 @@ clean() {
 
 
 # Check for proper command line arguments
-if [ -z $1 ]
+if [ -z $1 ] || [ -z $2 ]
 then
-  echo "Please provide a username."
+  echo "Please provide a username and domain name or IP."
   exit 1
 elif [ "$1" = "clean" ]
 then
@@ -64,19 +65,41 @@ printf "\n\t--bind unix:/run/gunicorn.sock \\" >> $servicefile
 printf "\n\tserver.main:app \n\n" >> $servicefile
 printf "[Install]\nWantedBy=multi-user.target\n" >> $servicefile
 
+# Create nginx config file
+printf "server {\n" > $nginxfile
+printf "\tlisten 80;\n" >> $nginxfile
+printf "\tserver_name $2;\n\n" >> $nginxfile
+printf "\tlocation = /favicon.ico { access_log off; log_not_found off; }\n" >> $nginxfile
+printf "\tlocation /static/ {\n" >> $nginxfile
+printf "\t\troot $(pwd);\n" >> $nginxfile
+printf "\t}\n\n" >> $nginxfile
+printf "\tlocation / {\n" >> $nginxfile
+printf "\t\tinclude proxy_params;\n" >> $nginxfile
+printf "\t\tproxy_pass http://unix:/run/gunicorn.sock;\n" >> $nginxfile
+printf "\t}\n" >> $nginxfile
+printf "}\n" >> $nginxfile
+
 
 # Additional requirements
-echo "NOTE: Please load venv and install packages"
+echo "NOTE: Manually run the following commands to finish setup."
+echo "WARNING: Requires sudo access to write service files.\n"
+
+echo "Load venv and install packages"
 echo "  source .venv/bin/activate"
 echo "  pip install -r requirements.txt"
 echo "  pip install gunicorn\n"
 
-# Copy over the service files
-echo "WARNING: Requires sudo access to write service files."
-echo "  sudo cp $socketfile $servicefile $servicedir\n"
+# Copy over the service and config files
+echo "Copy service and config files:"
+echo "  sudo cp $socketfile $servicefile /etc/systemd/system/"
+echo "  sudo cp $nginxfile /etc/nginx/sites-available/\n"
 
 # Start the gunicorn socket
-echo "Start gunicorn service with:"
+echo "Start gunicorn service:"
 echo "  sudo systemctl start gunicorn"
-echo "  sudo systemctl enable gunicorn"
+echo "  sudo systemctl enable gunicorn\n"
+
+# Setup nginx
+echo "Setup nginx:"
+echo "  sudo ln -s /etc/nginx/sites-available/$nginxfile /etc/nginx/sites-enabled"
 
